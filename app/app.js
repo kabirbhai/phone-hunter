5e2d7a3ffce5e8c704622ebf849ad092de10a26b
kabir <makkobir79@gmail.com>
2022-03-01 19:46:54 +0600

    added

--- app/app.js
+++ app/app.js
@@ -1,29 +1,34 @@
 const searchPhone = () => {
     const inputValue = document.getElementById('input').value;
+    document.getElementById('input').value = '';
     const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue}`
     fetch(url)
         .then(res => res.json())
         .then(data => displayPhone(data.data))
 }
 const displayPhone = phones => {
+    const displayField = document.getElementById('display-field')
+    displayField.textContent = '';
     for (const phone of phones) {
-        const displayField = document.getElementById('display-field')
         const div = document.createElement('div')
         div.innerHTML = `
         <div class="card">
             <img src="${phone.image}" alt="loading">
             <h3>Name: ${phone.phone_name}</h3>
             <h3>Brand: ${phone.brand}</h3>
+            <button>Detailes</button>
         </div>
         <div class="card">
             <img src="${phone.image}" alt="loading">
             <h3>Name: ${phone.phone_name}</h3>
             <h3>Brand: ${phone.brand}</h3>
+            <button>Detailes</button>
         </div>
         <div class="card">
             <img src="${phone.image}" alt="loading">
             <h3>Name: ${phone.phone_name}</h3>
             <h3>Brand: ${phone.brand}</h3>
+            <button>Detailes</button>
         </div>
         `
         displayField.appendChild(div)
@@ -31,3 +36,5 @@ const displayPhone = phones => {
 }
 
 // DISPLAY SEARCH PHONE END ------------------------------
+
+// details section start 
