ccac9d1679f65b3c393c3bb0b4b388a4c304d2a7
kabir <makkobir79@gmail.com>
2022-03-01 17:45:12 +0600

    display section finish

--- app/app.js
+++ app/app.js
@@ -7,6 +7,27 @@ const searchPhone = () => {
 }
 const displayPhone = phones => {
     for (const phone of phones) {
-        console.log(phone)
+        const displayField = document.getElementById('display-field')
+        const div = document.createElement('div')
+        div.innerHTML = `
+        <div class="card">
+            <img src="${phone.image}" alt="loading">
+            <h3>Name: ${phone.phone_name}</h3>
+            <h3>Brand: ${phone.brand}</h3>
+        </div>
+        <div class="card">
+            <img src="${phone.image}" alt="loading">
+            <h3>Name: ${phone.phone_name}</h3>
+            <h3>Brand: ${phone.brand}</h3>
+        </div>
+        <div class="card">
+            <img src="${phone.image}" alt="loading">
+            <h3>Name: ${phone.phone_name}</h3>
+            <h3>Brand: ${phone.brand}</h3>
+        </div>
+        `
+        displayField.appendChild(div)
     }
 }
+
+// DISPLAY SEARCH PHONE END ------------------------------
